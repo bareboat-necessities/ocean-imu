de760326821f971bfbb895727c8f16406f79a244
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-03-30 02:01:08 -0400

    initial

--- src/wave_dir/KalmanWaveDirection.h
+++ src/wave_dir/KalmanWaveDirection.h
@@ -28,7 +28,7 @@ class EIGEN_ALIGN_MAX KalmanWaveDirection {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    KalmanWaveDirection(float initialOmega, float directionReportAlpha = 0.05f)
+    KalmanWaveDirection(float initialOmega, float directionReportAlpha = 0.1f)
         : omega(initialOmega),
           phase(0.0f),
           direction_report_alpha(directionReportAlpha),
