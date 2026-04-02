a1e7dac792300f915e8c587c76804ed7f490cac3
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-01 20:51:29 -0400

    384

--- plots/spectrum/draw_plots.sh
+++ plots/spectrum/draw_plots.sh
@@ -3,3 +3,4 @@
 python3 spectrum-plots.py
 python3 spectrum-estimates-plots.py --mode classic
 python3 spectrum-estimates-plots.py --mode wavelets
+python3 spectrum-sea_metrics.py --csv sea_metrics_from_spectrum_report.csv --out-fragment ../../doc/spectrum/sea_metrics_from_spectrum_table_fragment.tex-part
--- tests/spectrum/run_tests.sh
+++ tests/spectrum/run_tests.sh
@@ -2,6 +2,4 @@
 
 ./spectrum-estimator-sim
 ./spectrum-wavelets-estimator-sim
-
 ./sea-metrics-from-spectrum-test
-python3 ../../plots/spectrum/sea-metrics-report.py --csv sea_metrics_from_spectrum_report.csv --out-fragment ../../doc/spectrum/sea_metrics_from_spectrum_table_fragment.tex-part --out-main ../../doc/spectrum/sea_metrics_from_spectrum_table.tex
