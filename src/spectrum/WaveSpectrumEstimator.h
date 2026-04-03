c7a4a7421f01a384e9b036be1bd1d3f48016b009
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-04-02 23:06:36 -0400

    Update WaveSpectrumEstimator.h

--- src/spectrum/WaveSpectrumEstimator.h
+++ src/spectrum/WaveSpectrumEstimator.h
@@ -413,8 +413,18 @@ private:
             S_aa_true_arr[i] = std::max(0.0, S_aa_meas * inv_hp);
         }
 
-        last_lowfreq_cut_hz_ = estimate_lowfreq_cut_from_accel_(S_aa_true_arr, Tblk);
+        const double cut_est_hz =
+             estimate_lowfreq_cut_from_accel_(S_aa_true_arr, Tblk);
 
+        last_lowfreq_cut_hz_ = WaveSpectrumShared::asym_smooth_hz(
+             last_lowfreq_cut_hz_,
+             cut_est_hz,
+             0.22,
+             0.08,
+             0.18,
+             0.06
+        );
+        
         const double f_knee = std::max({
             reg_f0_hz,
             0.50 * last_lowfreq_cut_hz_,
