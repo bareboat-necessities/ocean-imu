cc0b492e18be7ac6ce1656af99a9035bbf19bf01
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-04-03 02:09:29 -0400

    Update WaveSpectrumEstimator_Wavelets.h

--- src/spectrum/WaveSpectrumEstimator_Wavelets.h
+++ src/spectrum/WaveSpectrumEstimator_Wavelets.h
@@ -30,7 +30,7 @@
 */
 
 template<int Nfreq = 32, int Nblock = 256>
-class EIGEN_ALIGN_MAX WaveSpectrumEstimator {
+class EIGEN_ALIGN_MAX WaveSpectrumEstimator_Wavelets {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
@@ -38,7 +38,7 @@ public:
     using Vec = Eigen::Matrix<double, Nfreq, 1>;
     using Biquad = WaveSpectrumShared::Biquad;
 
-    WaveSpectrumEstimator(double fs_raw_ = 200.0,
+    WaveSpectrumEstimator_Wavelets(double fs_raw_ = 200.0,
                           int decimFactor_ = 30,
                           bool hannEnabled_ = true)
         : fs_raw(fs_raw_), decimFactor(decimFactor_), hannEnabled(hannEnabled_)
