16782e0fef039c1e5fecbbf95fd3c98b621687f7
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-03 04:05:02 -0400

    removed spectral analysis code

--- plots/spectrum/draw_plots.sh
+++ plots/spectrum/draw_plots.sh
@@ -1,20 +1,4 @@
 #!/bin/bash -e
 
 python3 spectrum-plots.py
-python3 spectrum-estimates-plots.py --mode combined
-python3 spectrum-sea_metrics.py \
-  --csv sea_metrics_from_spectrum_report_classic.csv \
-  --full-metrics-name-value sea_metrics_from_spectrum_full_metrics_classic.txt \
-  --out-fragment ../../doc/spectrum/spectrum-sea_metrics-classic.tex-part \
-  --fragment-input spectrum-sea_metrics-classic.tex-part \
-  --title "SeaMetricsFromSpectrum Report (Classic/Goertzel Estimator)" \
-  --caption "SeaMetricsFromSpectrum validation from classic (Goertzel) estimator spectra." \
-  --label "tab:sea_metrics_from_spectrum_classic"
-python3 spectrum-sea_metrics.py \
-  --csv sea_metrics_from_spectrum_report_wavelets.csv \
-  --full-metrics-name-value sea_metrics_from_spectrum_full_metrics_wavelets.txt \
-  --out-fragment ../../doc/spectrum/spectrum-sea_metrics-wavelets.tex-part \
-  --fragment-input spectrum-sea_metrics-wavelets.tex-part \
-  --title "SeaMetricsFromSpectrum Report (Wavelets Estimator)" \
-  --caption "SeaMetricsFromSpectrum validation from wavelets estimator spectra." \
-  --label "tab:sea_metrics_from_spectrum_wavelets"
+
--- tests/spectrum/run_tests.sh
+++ tests/spectrum/run_tests.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
 
-./spectrum-estimator-sim
-./spectrum-wavelets-estimator-sim
-./spectrum-sea_metrics-test
+
