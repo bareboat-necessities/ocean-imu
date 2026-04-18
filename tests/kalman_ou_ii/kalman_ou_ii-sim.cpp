0ab3d2b34ddc3159a6e0d46aa598269f234d6a16
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-18 09:06:42 -0400

    upd

--- tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
+++ tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
@@ -150,9 +150,9 @@ static constexpr W3dFailureLimits FAIL_LIMITS{
     .err_limit_percent_z_jonswap = 10.1f,
     .err_limit_percent_z_pmstokes = 9.8f,
     .err_limit_yaw_deg = 3.0f,
-    .err_limit_percent_3d_jonswap = 40.0f,
+    .err_limit_percent_3d_jonswap = 39.0f,
     .err_limit_percent_3d_pmstokes = 40.0f,
-    .acc_z_bias_percent = 40.0f,
+    .acc_z_bias_percent = 37.0f,
     .bias_3d_percent = 650.0f,
 };
 
