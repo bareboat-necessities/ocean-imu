aa2ca729ee1dcfccd765db668fa40506aca8d75f
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-18 11:30:33 -0400

    attempt to improve

--- src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
+++ src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
@@ -837,7 +837,7 @@ private:
     float online_tune_warmup_sec_ = ONLINE_TUNE_WARMUP_SEC;
     float mag_delay_sec_          = MAG_DELAY_SEC;
 
-    float R_p0_xy_factor_ = 0.23f;
+    float R_p0_xy_factor_ = 0.28f;
     float P_factor_       = 1.5f;
 
     TrackingPolicy               tracker_policy_{};
