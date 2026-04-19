bea1abc40eafd53b448fc0276cf92419d119b90d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-19 11:48:42 -0400

    fixing unbounded init time

--- tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
+++ tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
@@ -149,7 +149,7 @@ private:
 static constexpr W3dFailureLimits FAIL_LIMITS{
     .err_limit_percent_z_jonswap   = 9.9f,
     .err_limit_percent_z_pmstokes  = 11.5f,
-    .err_limit_yaw_deg             = 2.9f,
+    .err_limit_yaw_deg             = 3.0f,
     .err_limit_percent_3d_jonswap  = 40.0f,
     .err_limit_percent_3d_pmstokes = 40.0f,
     .acc_z_bias_percent            = 150.0f,
