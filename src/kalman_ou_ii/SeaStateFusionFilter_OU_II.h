9379506a4edf9299ec4026142b1c9020a0153d1d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-19 12:44:05 -0400

    fixing unbounded init time

--- src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
+++ src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
@@ -983,7 +983,7 @@ public:
 
             constexpr int   TILT_INIT_MIN_SAMPLES          = 200; // @ 200 Hz
             constexpr int   TILT_INIT_MIN_FALLBACK_SAMPLES = 40;  // @ 200 Hz
-            constexpr float TILT_INIT_TIMEOUT_SEC          = 3.0f;
+            constexpr float TILT_INIT_TIMEOUT_SEC          = 0.3f;
 
             const bool enough_good_samples =
                 (tilt_init_count_ >= TILT_INIT_MIN_SAMPLES);
