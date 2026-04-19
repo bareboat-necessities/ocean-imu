8177cf50409d3476a1c70f6e55b6848dbd37626d
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2026-04-19 14:48:28 -0400

    tighter init

--- src/kalman_ou_iii/SeaStateFusionFilter_OU_III.h
+++ src/kalman_ou_iii/SeaStateFusionFilter_OU_III.h
@@ -1011,9 +1011,9 @@ public:
                 ++tilt_init_count_;
             }
 
-            constexpr int   TILT_INIT_MIN_SAMPLES          = 100; // @ 200 Hz
-            constexpr int   TILT_INIT_MIN_FALLBACK_SAMPLES = 20;  // @ 200 Hz
-            constexpr float TILT_INIT_TIMEOUT_SEC          = 1.2f;
+            constexpr int   TILT_INIT_MIN_SAMPLES          = 160; // @ 200 Hz
+            constexpr int   TILT_INIT_MIN_FALLBACK_SAMPLES = 40;  // @ 200 Hz
+            constexpr float TILT_INIT_TIMEOUT_SEC          = 2.0f;
 
             const bool enough_good_samples =
                 (tilt_init_count_ >= TILT_INIT_MIN_SAMPLES);
