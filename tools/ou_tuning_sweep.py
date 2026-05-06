fde9fc6e716ab1a6df9b8aba68643c0187047a39
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-06 10:05:03 -0500

    Update ou_tuning_sweep.py

--- tools/ou_tuning_sweep.py
+++ tools/ou_tuning_sweep.py
@@ -48,11 +48,11 @@ BOOT_SPECS = {
 OU_COMMON_SPECS = {
     "OU_TAU_COEFF": (0.60, 1.50, "log"),
     "OU_SIGMA_COEFF": (0.60, 1.60, "log"),
-    "OU_ACC_NOISE_FLOOR_SIGMA": (0.008, 1.00, "log"),
-    "OU_ADAPT_TAU_SEC": (0.08, 45.0, "log"),
-    "OU_ADAPT_EVERY_SECS": (0.005, 2.5, "log"),
-    "OU_FREQ_INPUT_CUTOFF_HZ": (0.03, 3.0, "log"),
-    "OU_ACC_BIAS_INIT_STD": (0.001, 1.20, "log"),
+    "OU_ACC_NOISE_FLOOR_SIGMA": (0.02, 0.20, "log"),
+    "OU_ADAPT_TAU_SEC": (1.0, 12.0, "log"),
+    "OU_ADAPT_EVERY_SECS": (0.05, 0.50, "log"),
+    "OU_FREQ_INPUT_CUTOFF_HZ": (0.15, 2.5, "log"),
+    "OU_ACC_BIAS_INIT_STD": (0.001, 0.08, "log"),
 }
 
 OU_II_EXTRA_SPECS = {
