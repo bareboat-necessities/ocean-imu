bc90ff91a8141051265d9bb1ef1c49a205e79173
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-15 22:25:17 -0400

    Update SeaStateFusionFilter_OU_II.h

--- src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
+++ src/kalman_ou_ii/SeaStateFusionFilter_OU_II.h
@@ -725,7 +725,7 @@ private:
 
     bool accel_bias_locked_ = true;
     int  mag_updates_applied_ = 0;
-    static constexpr int MAG_UPDATES_TO_UNLOCK = 400;
+    static constexpr int MAG_UPDATES_TO_UNLOCK = 180;
 
     bool   with_mag_;
     double time_;
