eab10e4308d102cede56839ad143793235716f79
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-16 14:48:57 -0400

    Update kalman_ou_ii-sim.cpp

--- tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
+++ tests/kalman_ou_ii/kalman_ou_ii-sim.cpp
@@ -310,7 +310,7 @@ static constexpr W3dFailureLimits FAIL_LIMITS{
     .err_limit_percent_3d_jonswap  = 34.5f,
     .err_limit_percent_3d_pmstokes = 35.0f,
     .acc_z_bias_percent            = 13.0f,
-    .bias_3d_percent               = 300.0f,
+    .bias_3d_percent               = 420.0f,
 };
 
 static constexpr W3dSummaryLabels SUMMARY_LABELS{
