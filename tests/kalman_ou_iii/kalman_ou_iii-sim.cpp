cdb2595a4bd7fa30468c0d3cd75a9b1892db01cc
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-17 10:41:46 -0400

    Update kalman_ou_iii-sim.cpp

--- tests/kalman_ou_iii/kalman_ou_iii-sim.cpp
+++ tests/kalman_ou_iii/kalman_ou_iii-sim.cpp
@@ -269,7 +269,7 @@ private:
 static constexpr W3dFailureLimits FAIL_LIMITS{
     .err_limit_percent_z_jonswap   = 9.70f,
     .err_limit_percent_z_pmstokes  = 7.85f,
-    .err_limit_yaw_deg             = 2.55f,
+    .err_limit_yaw_deg             = 2.50f,
     .err_limit_percent_3d_jonswap  = 42.0f,
     .err_limit_percent_3d_pmstokes = 44.0f,
     .acc_z_bias_percent            = 11.5f,
