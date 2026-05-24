c527b35d449787111261d65ca272928fb748ce2e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-24 14:10:55 -0400

    Update atomS3R_ins_pii_observer.ino

--- sensors/full_marine_ins/atomS3R_ins_pii_observer/atomS3R_ins_pii_observer.ino
+++ sensors/full_marine_ins/atomS3R_ins_pii_observer/atomS3R_ins_pii_observer.ino
@@ -657,7 +657,7 @@ class FusionApp {
                         dt_);
     }
 
-    roll_deg_ = -fusion_.rollDeg();
+    roll_deg_ = fusion_.rollDeg();
     pitch_deg_ = -fusion_.pitchDeg();
     yaw_mahony_deg_ = fusion_.yawDeg();
 
