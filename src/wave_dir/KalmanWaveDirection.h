247c2a6615882c51520f75fdef58e565e3c5b470
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-05-24 21:05:35 -0400

    Update KalmanWaveDirection.h

--- src/wave_dir/KalmanWaveDirection.h
+++ src/wave_dir/KalmanWaveDirection.h
@@ -29,7 +29,7 @@ class EIGEN_ALIGN_MAX KalmanWaveDirection {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    KalmanWaveDirection(float initialOmega, float directionReportAlpha = 0.03f)
+    KalmanWaveDirection(float initialOmega, float directionReportAlpha = 0.01f)
         : omega(initialOmega),
           phase(0.0f),
           direction_report_alpha(directionReportAlpha),
