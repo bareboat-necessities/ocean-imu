b55a15a8809398eb537b1f562a3fd97b93710e6e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-08-04 12:18:02 -0400

    Adjust failure limits in kalman_ou_iii-sim.cpp
    Updated failure limits for simulation parameters.

--- tests/kalman_ou_iii/kalman_ou_iii-sim.cpp
+++ tests/kalman_ou_iii/kalman_ou_iii-sim.cpp
@@ -391,13 +391,13 @@ private:
 };
 
 static constexpr W3dFailureLimits FAIL_LIMITS{
-    .err_limit_percent_z_jonswap   = 9.68f,
-    .err_limit_percent_z_pmstokes  = 7.59f,
-    .err_limit_yaw_deg             = 2.21f,
-    .err_limit_percent_3d_jonswap  = 41.5f,
-    .err_limit_percent_3d_pmstokes = 44.2f,
-    .acc_z_bias_percent            = 13.2f,
-    .bias_3d_percent               = 283.0f,
+    .err_limit_percent_z_jonswap   = 6.6f,
+    .err_limit_percent_z_pmstokes  = 6.6f,
+    .err_limit_yaw_deg             = 2.2f,
+    .err_limit_percent_3d_jonswap  = 32.0f,
+    .err_limit_percent_3d_pmstokes = 26.0f,
+    .acc_z_bias_percent            = 8.2f,
+    .bias_3d_percent               = 275.0f,
 };
 
 static constexpr W3dSummaryLabels SUMMARY_LABELS{
