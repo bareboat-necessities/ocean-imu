a50419d6e81537cb17f2549a019e7a3c57aac54b
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-09-14 19:52:24 -0400

    proof(alt): consume admitted Mahony startup invariant

--- tools/stability/ou3_alt_contraction/finite_admitted_startup_machine_tunestate_word.py
+++ tools/stability/ou3_alt_contraction/finite_admitted_startup_machine_tunestate_word.py
@@ -28,6 +28,7 @@ from tools.stability.ou3_alt_contraction import finite_admitted_wpe_tau_interlea
 from tools.stability.ou3_alt_contraction import finite_admitted_machine_tunestate_interleaved_prefix as MACHINE
 from tools.stability.ou3_alt_contraction import finite_source_bound_live_word as WORD
 from tools.stability.ou3_alt_contraction import finite_tuner_deployment_config as DEPLOY
+from tools.stability.ou3_alt_contraction import admitted_startup_mahony_invariant as MAHONY
 
 QUALIFICATION='OU3_ALT_ADMITTED_STARTUP_MACHINE_TUNESTATE_WORD_V1'
 
@@ -68,6 +69,8 @@ def build(go:GO.Result, magnetic:MAG.CertifiedStartupState,
 
 def readiness():
     a=ADLIVE.readiness(); g=GO.readiness(); m=MACHINE.readiness()
+    mah=MAHONY.build(); mf=MAHONY.validate(mah)
+    if mf: raise RuntimeError('admitted startup Mahony invariant invalid: '+repr(mf))
     return {
       'dual_clock_magnetic_startup_and_admitted_BRMM_BIAS_handoff_consumed': bool(
           a['actual_startup_interleave_constructor_consumed_by_admitted_source_factory'] and
@@ -83,6 +86,9 @@ def readiness():
       'fresh_H18_reference_equals_same_admitted_tL_origin':a['startup_fresh_H18_reference_must_equal_admitted_tL_origin'],
       'same_goLive_state_seeds_admitted_600_edge_machine_product':m['whole_machine_goLive_provenance_constructor_available'],
       'startup_to_Live_machine_history_attachment_closed':True,
+      'admitted_source_private_Mahony_startup_to_Live_invariant_closed':
+          mah['admitted_source_private_Mahony_startup_to_Live_invariant_closed'],
+      'Mahony_invariant_no_longer_blocks_startup_finite_master':True,
       'every_admitted_startup_history_reaches_this_boundary_product':False,
       'source_uniform_startup_deployment_supply_bounds_closed':False,
       'all_target_libm_and_compiler_profile_correspondence_closed':False,
