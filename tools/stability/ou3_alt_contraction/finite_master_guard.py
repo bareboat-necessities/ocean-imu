2f73a3611652c41b9f17d2d7c8995b67a663693e
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-09-14 20:10:22 -0400

    proof(alt): consume dual WPE moment-log product

--- tools/stability/ou3_alt_contraction/finite_master_guard.py
+++ tools/stability/ou3_alt_contraction/finite_master_guard.py
@@ -16,6 +16,7 @@ from tools.stability.ou3_alt_contraction import finite_binary32_mahony_startup a
 from tools.stability.ou3_alt_contraction import finite_scheduler_nextafter_binary32 as NEXT
 from tools.stability.ou3_alt_contraction import finite_aw_sync_clock_binary64 as AWCLOCK
 from tools.stability.ou3_alt_contraction import finite_wpe_moment_binary32 as WPEMOM
+from tools.stability.ou3_alt_contraction import finite_wpe_machine_binary32 as WPEMACHINE
 from tools.stability.ou3_alt_contraction import finite_wpe_log_binary32 as WPELOG
 from tools.stability.ou3_alt_contraction import finite_wpe_frequency_binary32 as WPEFREQ
 from tools.stability.ou3_alt_contraction import finite_qaxis_exp_binary32 as QEXP
@@ -26,7 +27,7 @@ QUALIFICATION='OU3_ALT_FINITE_MASTER_GUARD_V1'
 def build():
     startup=START.readiness(); seed=STARTSEED.readiness()
     live=LIVE.readiness(); nxt=NEXT.readiness(); aw=AWCLOCK.readiness()
-    wm=WPEMOM.readiness(); wlog=WPELOG.readiness(); wfreq=WPEFREQ.readiness(); qexp=QEXP.readiness()
+    wm=WPEMOM.readiness(); wmach=WPEMACHINE.readiness(); wlog=WPELOG.readiness(); wfreq=WPEFREQ.readiness(); qexp=QEXP.readiness()
 
     closed={
       'startup_to_Live_machine_history_attachment': startup['startup_to_Live_machine_history_attachment_closed'],
@@ -42,6 +43,13 @@ def build():
           wm['all_raw_period_early_return_branches_materialized'] and
           wm['raw_period_has_no_independent_input_port'] and
           wm['raw_period_derived_from_same_machine_moments_and_sqrt_result']),
+      'WPE_dual_compiler_moment_raw_log_persistent_product': bool(
+          wmach['dual_compiler_persistent_moment_history_attached'] and
+          wmach['WPE_raw_period_binary32_production_attached_to_log_history'] and
+          wmach['raw_period_bound_to_same_mode_std_log_input']),
+      'WPE_repeated_period_exp_bound_to_same_stored_log_state': bool(
+          wmach['moment_horizon_period_bound_to_same_stored_log_state'] and
+          wmach['same_exp_log_period_value_bound_across_horizon_and_log_smoothing']),
     }
 
     open_obligations={
@@ -49,9 +57,10 @@ def build():
       'universal_startup_source_and_branch_reachability': not startup['every_admitted_startup_history_reaches_this_boundary_product'],
       'startup_source_uniform_deployment_supplies': not startup['source_uniform_startup_deployment_supply_bounds_closed'],
       'target_libm_and_compiler_profile_correspondence': not startup['all_target_libm_and_compiler_profile_correspondence_closed'],
-      'WPE_moment_exp_sqrt_target_libm_and_compiler_selection': not (wm['target_exp_sqrt_libm_correspondence_closed'] and wm['compiler_profile_selection_closed']),
-      'WPE_dual_compiler_persistent_moment_history_attachment': not wm['dual_compiler_persistent_moment_history_attached'],
-      'WPE_canonical_period_horizon_and_vertical_input_ancestry': not (wm['canonical_period_horizon_ancestry_closed'] and wm['same_machine_vertical_input_ancestry_closed']),
+      'WPE_machine_target_libm_and_compiler_selection': not (wmach['target_exp_log_sqrt_libm_correspondence_closed'] and wmach['compiler_profile_selection_closed']),
+      'WPE_startup_frontend_vertical_ancestry': not wmach['startup_frontend_vertical_ancestry_attached'],
+      'WPE_Live_600_step_machine_history_attachment': not wmach['Live_600_step_WPE_machine_history_attached'],
+      'WPE_source_uniform_machine_supply_bounds': not wmach['source_uniform_WPE_machine_supply_bounds_closed'],
       'WPE_log_and_exp_libm_correspondence': not (wlog['WPE_log_std_log_target_libm_correspondence_closed'] and wlog['WPE_log_exp_target_libm_correspondence_closed']),
       'WPE_frequency_source_uniform_supply': not wfreq['source_uniform_WPE_frequency_supply_bound_closed'],
       'Qaxis_exp_libm_correspondence': not qexp['Qaxis_exp_libm_binary32_correspondence_closed'],
